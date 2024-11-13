22a679e30dbc074885fa4206bf9ee9a288e277e4
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-11-14 10:56:26 +1100

    comments

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ dotenv.config(); // Load environment variables
 
 const googleSheetId = process.env.GOOGLE_SHEET_ID;
 
+// Read and process the Testing sheet
 const groupedRfiData = await processTestingSheet(googleSheetId, "Testing");
 
+// Update the RFI Spreadsheet with the groupedRFI data from the Testing sheet
 updateRfiSpreadsheet(googleSheetId, "RFI Spreadsheet", groupedRfiData);
