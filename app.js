f2329ec54d9e7a76d6dc68820008c33d9f268c8f
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-11-14 10:50:22 +1100

    Rename function updateGoogleSheet

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
-import { processGoogleSheet, updateSheet } from "./lib/utils.js";
+import { processGoogleSheet, updateGoogleSheet } from "./lib/utils.js";
 
 dotenv.config(); // Load environment variables
 
 const googleSheetId = process.env.GOOGLE_SHEET_ID;
-const googleSheetPage = process.env.GOOGLE_SHEET_PAGE;
 
-processGoogleSheet(googleSheetId, googleSheetPage);
+const groupedRfiData = await processGoogleSheet(googleSheetId, "Testing");
+updateGoogleSheet(googleSheetId, "RFI Spreadsheet", groupedRfiData);
